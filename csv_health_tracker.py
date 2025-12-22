c9ce7c00161d675835eba2b83956f7bf2d929081
Nikhil Adhikari <nikhiladhikari1@gmail.com>
2025-12-22 15:40:57 +0100

    Added 2 exception handling

--- csv_health_tracker.py
+++ csv_health_tracker.py
@@ -24,16 +24,56 @@ csv_file_path = input()
 
 
 print("CSV Health Checker")
-print("--------------------")
+print("This tool provides you with the comprehensive summary of the csv file file before it goes further down the pipeline.")
 print("Provide the path to the CSV file you want to validate.")
 print("Example: C:/Users/YourName/Desktop/data.csv")
 print()
 
 
+
+"""
 csv_file_path = input("Enter csv file path: ")
 
 if not csv_file_path:
     print("The filename is empty. Please enter the csv file name.")
+
+ """
+
+import os # needed for validating the file path and file exceptions 
+
+# Before we ask for the filepath to the user we need to provide them with the current directory that we are in 
+# since we want relative path to locate the file that the user is giving
+
+current_directory = os.getcwd()
+print(f"This is our current directory - {current_directory} . Please provide the path to you file while considering the current directory ")
+
+user_file_path = input()
+
+# After taking the file path we want to make sure that the file exists
+# Here we will be specifically checking if the path provided leads to file or not
+
+
+print(f"\n Checking if '{user_file_path}' exists...")
+
+# Checking if the user provided us with empty input
+
+if not user_file_path:
+    print("The given input is empty. Please enter the file path to the CSV file.")
+
+if os.path.exists(user_file_path):
+    print(f"The given file exists. Now we will begin further processing.")
+else:
+    print(f" The given file doesnot exist. Please make sure that you gave the correct file path")
+
+
+# Till now what we have basically done is checck whther the input is empty 
+# as well as whether the given path leads to file or not
+# now we need to make sure that the file is csv 
+
+
+# using pandas to do operations on the file
+
+import pandas as pd
 
 
 
